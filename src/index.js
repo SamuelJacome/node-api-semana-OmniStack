68e299c455d4bc308e82a18a4b6711d7be830ac2
SamuelJacome <samuelvictor@rede.ulbra.br>
2020-03-24 18:29:05 -0300

    add cors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors')
 const routes = require('./routes')
 
 const app = express();  
 
+app.use(cors());
 app.use(express.json()) // Isso deve ser feito antes de todas as rotas(Configuração para )
 app.use(routes);
 app.listen(3333);

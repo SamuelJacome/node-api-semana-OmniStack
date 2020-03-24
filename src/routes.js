bb2f815cd367466ab941adf0d41af9a9077b4252
SamuelJacome <samuelvictor@rede.ulbra.br>
2020-03-24 11:57:48 -0300

    Correção Table Ongs_

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,25 @@
 const express = require('express');
+const cryto = require('crypto');
+const connection = require('./database/connection');
 
 const routes = express.Router();
 
+routes.post('/ongs', async (req, res) => {
+    const { name, email, whatsapp, city, uf } = req.body;
 
+    const id = cryto.randomBytes(4).toString('HEX');
 
-routes.post('/users', (req, res) =>{
-     const body = req.body;
-     console.log(body);
+    await connection('ongs_').insert({
+        id,
+        name,
+        email,
+        whatsapp,
+        city,
+        uf,
+    });
  
-     return res.json({
-         evento: "Semana Omnistack",
-         aluno: "Samuel Jácome",
-     })
- })
+     return res.json({ id });
+     
+});
 
  module.exports = routes;
